ebfd69e124b6f460a26e49dff6557778f868c401
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-09-03 17:05:48 +1000

    Fixes missing App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@chakra-ui/react';
+import Form from './features/form/form';
+
+const App = () => {
+    return (
+        <Container>
+            <Form />
+        </Container>
+    );
+};
+
+export default App;
