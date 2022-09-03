7b775de5354832755e8a44dc181bfa292c300b25
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2022-09-03 16:51:09 +1000

    Migrates from Formik > Redux Toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Provider>
   </React.StrictMode>
 );
